b6d624922a70c6fd1df7e85dfdd22587c48b6394
Allison Park <a2park@ucsd.edu>
2024-10-31 00:49:53 -0700

    Update tictac_basic.py
    Removed extra comment lines

--- tictac_basic.py
+++ tictac_basic.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
 import numpy as np
 import random
 
@@ -857,3 +852,4 @@ if not state:
             playboard.printTurn(player1, computer)
 
     print("The game has ended. Thanks for playing!")
+    
